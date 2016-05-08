9e7fa15699c866e520e6cb3fef3793d881f5d29c
tabasano <pianotoieba@gmail.com>
2016-05-08 15:47:36 +0900

    not to make short span

--- wav-split.rb
+++ wav-split.rb
@@ -7,16 +7,20 @@ require 'optparse'
 require 'rubygems'
 require 'wav-file'
 
+
+extra=6
 max=1000
 reptime=1
 sflag=true
 outdir=false
 opt = OptionParser.new
 opt.on('-x',"dont save") {|v| sflag=false }
+opt.on('-e v',"extra num to omit too short spans") {|v| extra=v.to_i }
 opt.on('-m v',"max") {|v| max=v.to_i }
 opt.on('-d v',"out dir") {|v| outdir=v }
 opt.on('-j',"out-join mode") {|v| $join=true }
 opt.on('-t v',"repeat time") {|v| reptime=v.to_i }
+opt.on('-s',"show raw mode") {|v| $showraw=true }
 opt.parse!(ARGV)
 
 
@@ -88,22 +92,31 @@ chwav=f2data(chime)[1]
 copos=checklevel(wavs,200,st)
 p :co,copos.size,:co_sort,copos[-200..-1] if $DEBUG
 
-n=max
-if copos.size>max
-  n=max
+n=copos.size
+if copos.size>max+extra
+  n=max+extra
 end
 # sort by count => sort by position
 spl=copos[-n..-1].map{|c,pos|pos}.sort
 
-p spl.size
+p [:copos,copos.size,:spl,spl.size]
 p spl if $DEBUG
-play=[0]
+# [position,step from previous]
+play=[[0,0]]
 base=1000
 (spl.size-1).times{|i|
-  play<<spl[i+1] if spl[i+1]-play[-1] > base
+  step=spl[i+1]-play[-1][0]
+  play<<[spl[i+1],step] if step > base
 }
-play<<wavs.size-1 if play[-1]!=wavs.size-1
+rest=wavs.size-play[-1][0]
+play<<[wavs.size,rest] if play[-1][0]!=wavs.size
 
+# reject too short spans
+minus=play.sort_by{|po,st|st}[0..extra]
+p [:extra,extra],minus
+minus=minus.map{|po,st|po}-[0,wavs.size]
+play.reject!{|po,st|minus.member?(po)}
+p [:max,play[-1],play[-2],"...",play[1],play[0]]
 
 def save f,format,dataChunk
   print"save!" if $DEBUG
@@ -114,23 +127,32 @@ def save f,format,dataChunk
   print"..\n" if $DEBUG
 end
 p [:size, spl.size, play.size,($join ? :join : :not_join)]
-spl=play
+tmp=play.map{|pos,step|step}
+spzero,spmin,splast,spmax=tmp[0],tmp[1],tmp[-2],tmp[-1]
+p [:span_minmax,spmin,spmax,spzero,splast]
+spl=play.map{|pos,step|pos/2*2}
 p :save_start
 
 form="%0#{spl.size>100 ? 4 : 3}d"
 wavtmp=[]
 pkd=""
+log=[]
+log<<[:wav,wavs.size]
+pksize=0
+unit=1000
+unit=1 if $showraw
 (spl.size-1).times{|i|
   st,en=spl[i],spl[i+1]
   wavtmp=[]
+  tmpsize=0
   pkd="" if ! $join
-  wpkd=wavs[st..en].pack(bit)
+  wpkd=(tmp=wavs[st...en];tmpsize=tmp.size;pksize+=tmpsize;tmp).pack(bit)
   cpkd=chwav.pack(bit)
   reptime.times{|i|
     pkd+=wpkd
     pkd+=cpkd if i<reptime-1
   }
-  print (en-st)/1000,","
+  print tmpsize/unit,","
   num=format(form,i)
   name="#{file}_split-#{num}.wav"
   name="#{outdir}/#{File.basename(name)}" if outdir
@@ -139,10 +161,11 @@ pkd=""
     save name,format,dataChunk
   end
 }
+log<<[:pksize,pksize]
 if sflag && $join
   name="#{file}_split-join.wav"
   name="#{outdir}/#{File.basename(name)}" if outdir
   dataChunk.data = pkd
   save name,format,dataChunk
 end
-
+p log
